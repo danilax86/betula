349392be0730c32ef625829ba3a4d51137458b1f
Danila Gorelko <danila@danilax86.space>
2024-06-06 15:48:15 +0000

    Add check for package comments

--- tools/slice.go
+++ tools/slice.go
@@ -1,15 +1,15 @@
+// Package tools provides common functions.
 package tools
 
-
 func insertElement[T any](array []T, value T, index int) []T {
-    return append(array[:index], append([]T{value}, array[index:]...)...)
+	return append(array[:index], append([]T{value}, array[index:]...)...)
 }
 
 func removeElement[T any](array []T, index int) []T {
-    return append(array[:index], array[index+1:]...)
+	return append(array[:index], array[index+1:]...)
 }
 
 func MoveElement[T any](array []T, srcIndex int, dstIndex int) []T {
-    value := array[srcIndex]
-    return insertElement(removeElement(array, srcIndex), value, dstIndex)
+	value := array[srcIndex]
+	return insertElement(removeElement(array, srcIndex), value, dstIndex)
 }
